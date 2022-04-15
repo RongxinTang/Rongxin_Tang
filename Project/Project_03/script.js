814f148a4d94d3639cf893bf2181e306729c7237
RongxinTang <rt2820@columbia.edu>
2022-04-15 01:07:57 -0400

    project 3 update 0414

--- Project/Project_03/script.js
+++ Project/Project_03/script.js
@@ -1,3 +1,4 @@
+//Data sourse
 const materials = [
     {
         name:"Moss",
@@ -11,9 +12,84 @@ const materials = [
         color: "Green",
         image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909279/Materials%20in%20my%20city/IMG_8197_yb0zy2.jpg",
     },
+    {
+        name: "Chili",
+        type: "Food",
+        color: "Red",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909286/Materials%20in%20my%20city/IMG_8194_uyqdd4.jpg",
+    },
+    {
+        name: "Cracked Mud Wall",
+        type: "Wall",
+        color: "Khaki",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909282/Materials%20in%20my%20city/IMG_8097_pw1vro.jpg",
+    },
+    {
+        name: "Two-color Brick Wall",
+        type: "Wall",
+        color: "Grey",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909280/Materials%20in%20my%20city/IMG_8096_st8ace.jpg",
+    },
+    {
+        name: "Brick Wall",
+        type: "Wall",
+        color: "Grey",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909275/Materials%20in%20my%20city/IMG_8091_swtgu4.jpg",
+    },
+    {
+        name: "Plank",
+        type: "Wall",
+        color: "Grey",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909268/Materials%20in%20my%20city/IMG_8095_yal9vz.jpg",
+    },
+    {
+        name: "Wooden Door",
+        type: "Wall",
+        color: "Red",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909266/Materials%20in%20my%20city/IMG_8094_quwkfq.jpg",
+    },
+    {
+        name: "Mud Wall",
+        type: "Wall",
+        color: "Khaki",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909265/Materials%20in%20my%20city/IMG_8085_rekady.jpg",
+    },
+    {
+        name: "Red Plank",
+        type: "Wall",
+        color: "Red",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909259/Materials%20in%20my%20city/IMG_8105_ay3mo7.jpg",
+    },
+    {
+        name: "Iron Gate",
+        type: "Wall",
+        color: "Red",
+        image: "https://res.cloudinary.com/drvfsh5ig/image/upload/v1649909251/Materials%20in%20my%20city/IMG_4792_m7ow2h.jpg",
+    },
 ];
 
 
+//Add collapsable sorting menu
+
+var collBtn = document.getElementsByClassName("collapsible");
+
+for (var i = 0; i < collBtn.length; i++) {
+  function expand(){
+    this.classList.toggle("active");
+    let collContent = this.nextElementSibling;
+    if (collContent.style.display === "block"){
+      collContent.style.display = "none";
+    } else {
+      collContent.style.display = "block";
+    }
+  }
+  
+  collBtn[i].addEventListener("click", expand);
+  
+}
+
+
+//Render materials to the page
 const cardContainer = document.querySelector(".cardContainer");
 
 function renderMaterialToPage(materials) {
@@ -44,3 +120,8 @@ function renderMaterialToPage(materials) {
 }
 
 renderMaterialToPage(materials);
+
+
+//filter by type
+
+let typeFilter = document.querySelector(".")
